6789c0af4b0294c1e6eb485bf4e30903c9c836c4
martin dussenneMartin Dussenne <martin.dussenne@gmail.com>
2025-11-05 14:51:22 +0100

    MVP de chez MVP (sans front-end)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0487412413", category: "belgian")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0487411413", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "MacDo", address: "Avenue de la citronelle 26 1340 Ottignies", phone_number: "0497411413", category: "italian")
+puts "Created McDo"
+Restaurant.create!(name: "Peniche", address: "rue de la boulette 10 1360 Wavre", phone_number: "0487000000", category: "french")
+puts "Peniche"
+Restaurant.create!(name: "Chez Didier", address: "rue de la cendre 10 1360 Jodoigne", phone_number: "0487999999", category: "chinese")
+puts "Chez Didier"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
